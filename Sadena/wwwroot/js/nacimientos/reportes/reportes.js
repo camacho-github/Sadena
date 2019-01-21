c9afbd6188c0dd29e75423d5b1b36ec7704f4512
Ricardo Camacho <rcamacho10m@gmail.com>
2019-01-21 01:31:01 -0600

    Cambios en Web para mapas y reportes

--- Sadena/wwwroot/js/nacimientos/reportes/reportes.js
+++ Sadena/wwwroot/js/nacimientos/reportes/reportes.js
@@ -2,6 +2,23 @@
 
 $(function () {
 
+    $('#coahuilaMap').vectorMap({
+        map: 'CoahuilaDeZaragozaMap',
+        backgroundColor: '#a5bfdd',
+        borderColor: '#818181',
+        borderOpacity: 0.25,
+        borderWidth: 1,
+        color: '#f4f3f0',
+        enableZoom: true,
+        hoverColor: '#c9dfaf',
+        hoverOpacity: null,
+        normalizeFunction: 'linear',
+        scaleColors: ['#b6d6ff', '#005ace'],
+        selectedColor: '#c9dfaf',
+        selectedRegion: null,
+        showTooltip: true
+    });
+
     /* Unregistered table */
     $('#unregisteredTable').DataTable({
         'scrollX': true,
